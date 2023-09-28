c399ae98ae8553ae602c385e6053d97535435e2e
Mateusz Banaszak <matb@mapspeople.com>
2023-09-28 11:17:15 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mapbox-3-gl';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  constructor() {}
+  map: mapboxgl.Map;
+  @ViewChild('mapElement') mapElement: ElementRef;
+
+  ngOnInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbGNrNWZsM3YwYzdwM3ZwODd3bXhjMmU4In0.ZtFUUSvc-nk9xJD_dyuA5w';
+  }
+
+  ngAfterViewInit(){
+    this.map = new mapboxgl.Map({
+        container: this.mapElement.nativeElement,
+    });
+}
 }
